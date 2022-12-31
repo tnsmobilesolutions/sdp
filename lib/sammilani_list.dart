6c7701dce4ff0cd4c1b17ffc0d7ab68b6131766a
Dhananjay moharana <csdhananjaymoharana@gmail.com>
2023-01-01 02:48:40 +0530

    sammilani list

---
+++ lib/sammilani_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class SammilaniList {
+  SammilaniList(
+      {this.sammilaniNumber, this.sammilaniYear, this.sammilaniPlace});
+  int? sammilaniYear;
+  String? sammilaniPlace;
+  int? sammilaniNumber;
+}
+
+final allSammilaniList = [
+  SammilaniList(
+      sammilaniNumber: 1,
+      sammilaniYear: 1947,
+      sammilaniPlace: 'Ankoli, Berhampur'),
+  SammilaniList(
+      sammilaniNumber: 2,
+      sammilaniYear: 1948,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 3,
+      sammilaniYear: 1949,
+      sammilaniPlace: 'Banahara,Gopa, Puri'),
+  SammilaniList(
+      sammilaniNumber: 4,
+      sammilaniYear: 1950,
+      sammilaniPlace: 'Digapahandi, Ganjam'),
+  SammilaniList(
+      sammilaniNumber: 5,
+      sammilaniYear: 1951,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 6,
+      sammilaniYear: 1952,
+      sammilaniPlace: 'Erabanga,Gopa, Puri'),
+  SammilaniList(
+      sammilaniNumber: 7,
+      sammilaniYear: 1953,
+      sammilaniPlace: 'Padmapura,Sheragarh,Ganjam'),
+  SammilaniList(
+      sammilaniNumber: 8,
+      sammilaniYear: 1954,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 9,
+      sammilaniYear: 1955,
+      sammilaniPlace: 'Biratunga, Puri'),
+  SammilaniList(
+      sammilaniNumber: 10,
+      sammilaniYear: 1956,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 11,
+      sammilaniYear: 1958,
+      sammilaniPlace: 'Biratunga, Puri'),
+  SammilaniList(
+      sammilaniNumber: 12,
+      sammilaniYear: 1959,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 13,
+      sammilaniYear: 1960,
+      sammilaniPlace: 'Kendupatana,Cuttack'),
+  SammilaniList(
+      sammilaniNumber: 14,
+      sammilaniYear: 1962,
+      sammilaniPlace: 'Chatara,Jagatsinghpur'),
+  SammilaniList(
+      sammilaniNumber: 15,
+      sammilaniYear: 1963,
+      sammilaniPlace: 'Biratunga,Puri(Formality)'),
+  SammilaniList(
+      sammilaniNumber: 17,
+      sammilaniYear: 1964,
+      sammilaniPlace: 'Jobra, Cuttack'),
+  SammilaniList(sammilaniNumber: 18, sammilaniYear: 1965, sammilaniPlace: ''),
+];
